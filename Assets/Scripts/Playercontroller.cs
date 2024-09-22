ae6a09f9a482a7ac552ee876b35a24f067ecd55c
trieuhuynh0107 <trieuhuynh010704@gmail.com>
2024-09-22 20:43:35 +0700

    Add gamemanager script and playercontroller scripts
    using Input to get the input from the player and vector2 to control the duck on horizontal axis

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Playercontroller : MonoBehaviour
 {
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+
+    private float moveX;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (moveX * moveSpeed, rb.velocity.y);
     }
 }
