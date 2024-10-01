7b585509f102565e69064bbb6fae7ca62dd04057
thuyhien254 <nguyenthuyhien25042004@gmail.com>
2024-10-01 21:18:27 +0700

    Script for Gamemanager
    Destroy the lifebuoy after the duck jump on

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Duck;
+    public GameObject lifebuoyPrefab;
+    private GameObject myLifebuoy;
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        myLifebuoy = (GameObject)Instantiate(lifebuoyPrefab, new Vector2(Random.Range(-5.5f, 5.5f), Duck.transform.position.y + (14 + Random.Range(0.5f, 1f))), Quaternion.identity);
+        Destroy(collision.gameObject);
+
+    }
+
+
 }
